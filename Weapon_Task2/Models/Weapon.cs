da4e3be5b51b776a506cbbcdd0b17b8fcbc9d38d
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-16 11:47:28 +0400

    Add project files.

---
+++ Weapon_Task2/Models/Weapon.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon_Task2
+{
+    class Weapon
+    {
+        private int _bulletCapacity;
+        private int _bulletCount;
+        private int _dischargeSeconds;
+        private bool _auto;
+
+        public Weapon (int bulletCapacity, int bulletCount, double dischargeSeconds, bool auto)
+            {
+           
+            bulletCapacity = bulletCapacity;
+            bulletCapacity = bulletCount;
+            dischargeSeconds = dischargeSeconds;
+            auto = auto;
+
+            }
+                public void Shoot()
+                {
+                    if (_bulletCount > 0)
+                    {
+                        _bulletCount--;
+                        Console.WriteLine("1 bullet was fired");
+                    }
+                    else
+                    {
+                        Console.WriteLine("no bullet left");
+                    }
+                }
+                public void Fire()
+                {
+                    if (_bulletCount == 0)
+                    {
+                        Console.WriteLine("no bullets");
+                    }
+                    else
+                    {
+                        double time = _bulletCount * _dischargeSeconds / _bulletCapacity;
+                        Console.WriteLine(time + "san");
+                    }
+                }
+
+                public void GetRemainBulletCount()
+                {
+                    int required = 0;
+                    required = _bulletCapacity - _bulletCount;
+                    Console.WriteLine("How many bullets are left:" + required);
+                }
+                public void Reload()
+                {
+                    if (_bulletCapacity - _bulletCount == 0)
+                    {
+                        Console.WriteLine("the comb of the bullet is full");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{_bulletCapacity - _bulletCount}: + n add as many bullets");
+                    }
+                }
+                public void ChangeFireMode()
+                {
+                    bool choice = bool.Parse(Console.ReadLine());
+                    if (choice == true)
+                    {
+                        _auto = true;
+                    }
+                    else if (choice == false)
+                    {
+                        Console.WriteLine("There are no bullets in the weapon to make a choice");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Make one of two choices");
+                    }
+                }
+                public void GetInfo()
+                {
+                    Console.WriteLine($"bulletCapacity:{_bulletCapacity} bulletCount:{_bulletCount} dischargeSeconds:{_dischargeSeconds} auto:{_auto}");
+                }
+            }
+
+        }
---
+++ Weapon_Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Weapon_Task2;
+
+namespace Weapon_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter");
+            Console.WriteLine("Enter the bullet capacity");
+            int bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of bullets");
+            int bulletCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the discharge time of the bullet");
+            double dischargeSeconds = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Select the shooting mode");
+            bool auto = bool.Parse(Console.ReadLine());
+
+            Weapon Gun = new Weapon(bulletCapacity, bulletCount,dischargeSeconds,auto);
+            int choise = -1;
+            while (choise != 6)
+            {
+                choise = Convert.ToInt32(Console.ReadLine());
+                switch (choise)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Gun.Shoot();
+                        break;
+                    case 2:
+                        Gun.Fire();
+                        break;
+                    case 3:
+                        Gun.GetRemainBulletCount();
+                        break;
+                    case 4:
+                        Gun.Reload();
+                        break;
+                    case 5:
+                        Gun.ChangeFireMode();
+                        break;
+                    case 6:
+                        Gun.Edit();
+                        break;
+                    case 7:
+                        Gun.Reset();
+                        break;
+                    case 8:
+                        Gun.GetInfo();
+                        break;
+                    default:
+                        Console.WriteLine("Thanks");
+                        break;
+                }
+            }
+        }
+    }
+}
